09aedb08b5df7ec0d966074c9c741fb3f9256824
JustinCresswell <150776975+JustinCresswell@users.noreply.github.com>
2023-11-15 14:39:49 -0700

    Add files via upload
    fully working song, manager, and main_dev

--- playlist/main_dev.py
+++ playlist/main_dev.py
@@ -4,7 +4,6 @@ from playlist import PlayList
 from manager import Manager
 import os
 
-
 def main():
     # Load all songs from a CSV into a song pool
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -13,14 +12,14 @@ def main():
 
     # Try to load the previous state if it exists
     manager = Manager.load_state("manager_state.pkl")
-
+    
     while True:
         if not manager.current_playlist:
             print("\n--- 🪩 Playlist Manager 🪩 ---")
             print("1. Create a new playlist 🟢")
             print("2. Select an existing playlist 🔢")
-            print("4. Save state 📥")
-            print("0. Exit 🛑")
+            print("4. Save state 📥")  
+            print("0. Exit 🛑")          
         else:
             print(f"\n--- 🎧 {manager.current_playlist.name} 🎧 ---")
             print("1. Play current song ▶")
@@ -31,12 +30,10 @@ def main():
             print("6. Go back to main menu ⬅️")
             print("7. Display available songs 🗒️")
             print("8. Save state 📥")
-            print("a. Autoplay Playlist")
-            print("u. Undo")
-            print("r. Redo")
-            print("s. Share")
+            print("9. Share Playlist >>>")
+            print('10. type "redo" or "undo" to revert any changes')
             print("0. Exit 🛑")
-
+            
             # total amount of time for the playlist by ⌛
             # total number of song in the playlist by 🔢
             # Check the Playlist to explore other functionalities to add ***
@@ -45,7 +42,7 @@ def main():
         except ValueError:
             print("\033[31mPlease enter a valid choice number!\033[0m")
             continue
-
+        
         if not manager.current_playlist:
             if choice == '1':
                 playlist_name = input("Enter name for the new playlist: ")
@@ -53,10 +50,9 @@ def main():
             elif choice == '2':
                 manager.select_or_create_playlist()
             elif choice == '4':
-                manager.save_state("manager_state.pkl")
+                manager.save_state("manager_state.pkl")    
         if choice == '0':
-            confirm_exit = input(
-                "Are you sure you want to exit? (y/n): ").lower()
+            confirm_exit = input("Are you sure you want to exit? (y/n): ").lower()
             if confirm_exit == 'y':
                 break
         else:
@@ -68,8 +64,7 @@ def main():
                 manager.play_previous_song()
             elif choice == '4':
                 song_name_or_id = input("Enter the song name or ID:")
-                manager.add_song_to_playlist_by_name_or_id(
-                    song_name_or_id, song_pool)
+                manager.add_song_to_playlist_by_name_or_id(song_name_or_id, song_pool)
             elif choice == '5':
                 manager.display_current_playlist_songs()
             elif choice == '6':
@@ -78,21 +73,17 @@ def main():
                 manager.display_available_songs(song_pool)
             elif choice == '8':
                 manager.save_state("manager_state.pkl")
-            elif choice.lower() == 'a':
-                manager.autoplay()
-            elif choice.lower() == 'u':
-                manager.undo()
-            elif choice.lower() == 'r':
-                manager.redo()
-            elif choice.lower() == 's':
+            elif choice == '9':
                 manager.share_playlist()
+            elif choice.lower() == "redo":
+                manager.redo()
+            elif choice.lower() == "undo":
+                manager.undo()
         if choice == '0':
-            confirm_exit = input(
-                "Are you sure you want to exit? (y/n): ").lower()
+            confirm_exit = input("Are you sure you want to exit? (y/n): ").lower()
             if confirm_exit == 'y':
                 break
-        print("\n" + "-" * 40)
-
+        print("\n" + "-"*40)
 
 if __name__ == "__main__":
     main()
--- playlist/playlist.py
+++ playlist/playlist.py
@@ -1,105 +1,135 @@
 from song import Song
 import webbrowser
 
-
 class PlayList:
-
-    def __init__(self, name: str = 'Default') -> None:
-        # start of the song list [head of the doubly-linked list]
-        self.start: Song = None
-        # end of the song list [tail of the doubly-linked list]
-        self.end: Song = None
-        self.cur: Song = None  # cur song of the playlist
+    def __init__(self, name : str = 'Default') -> None:
+        self.start : Song = None  # start of the song list [head of the doubly-linked list]
+        self.end : Song = None    # end of the song list [tail of the doubly-linked list]
+        self.cur : Song = None    # cur song of the playlist
         self.name = name
         self.length = 0
+        self.redo_stack = []
+        self.undo_stack = []
 
     def _get_song_at_index(self, index) -> Song:
         """
-		Helper method to retrieve the song at a specific index.
+        Helper method to retrieve the song at a specific index.
 
-		Args:
-			index (int): index of the song to retrieve
+        Args:
+            index (int): index of the song to retrieve
 
-		Returns:
-			Song: song at the specified index
-		"""
+        Returns:
+            Song: song at the specified index
+        """
         # start from the beginning and keep going forward, finally return the desired song
         current = self.start
         count = 0
-        while current and count < index:
+        while current is not None and count < index:
+            print('test error in helper')
             current = current.next
             count += 1
         return current
 
+
     def remove_by_name(self, name: str) -> None:
         """
-		Remove the first occurrence of a song by its name.
-		- Traverse the list to find the song.
-		- Adjust the prev and next attributes of the surrounding songs accordingly.
+        Remove the first occurrence of a song by its name.
+        - Traverse the list to find the song.
+        - Adjust the prev and next attributes of the surrounding songs accordingly.
 
-		Args:
-			name (str): name of the song to remove
-		"""
-        current = self.start
-        while current:
-            if current.name == name:
-                if current == self.start:
-                    self.start = current.next
-                if current == self.end:
-                    self.end = current.prev
-                if current.prev:
-                    current.prev.next = current.next
-                if current.next:
-                    current.next.prev = current.prev
-                self.length -= 1
-                return
-            current = current.next
-        print(f'No item named {name} in playlist')
+        Args:
+            name (str): name of the song to remove
+        """
+        if self.start is not None:
+            current = self.start
+            while current is not None:
+                if current.name == name:
+                    if current is self.start and current is self.end:
+                        current.prev = None
+                        current.next = None
+                        self.clear()
+                        return
+                    elif current.next is None:
+                        print(current, current.next, current.prev)
+                        self.end = current.prev 
+                        self.end.next = None
+                        self.length -= 1
+                        current.prev = None
+                        current.next = None
+                        return
+                    else:
+                        current.next.prev = current.prev
+                        current.prev.next = current.next
+                        current.prev = None
+                        current.next = None
+                        self.length -= 1
+                        return
+                current = current.next
+            self.length -= 1
+            return "Song not found"
 
-    def remove_by_index(self, index: int) -> None:
+
+    def remove_by_index(self, index: int, name:str = None) -> None:
         """        
-		Remove the song at the specified index.
-		- Traverse the list to find the song at the given index.
-		- Adjust the prev and next attributes of the surrounding songs accordingly.
+        Remove the song at the specified index.
+        - Traverse the list to find the song at the given index.
+        - Adjust the prev and next attributes of the surrounding songs accordingly.
 
-		Args:
-			index (int): index of the song to remove
-		"""
+        Args:
+            index (int): index of the song to remove
+        """
         my_idx = index
         if my_idx < 0:
             my_idx += self.length
         if my_idx < self.length:
             song = self._get_song_at_index(index=my_idx)
-            song.prev.next = song.next
-            song.next.prev = song.prev
+            if song is None:
+                try:
+                    self.remove_by_name(name)
+                except:
+                    raise IndexError(f"Index out of bounds index {index} and length {self.length}")
+                return                
+            elif song is self.start:
+                if song.next is not None:
+                    self.start = self.start.next
+                    self.start.prev = None
+                else:
+                    self.clear()
+                    return
+            elif song is self.end:
+                song.prev.next = song.next
+                song.prev = None
+            else:
+                song.prev.next= song.next
+                song.next.prev = song.prev
+            
+            song.prev = None
+            song.next = None
         else:
-            raise IndexError("Index out of bounds")
-
+            try:
+                self.remove_by_name(name)
+            except:
+                raise IndexError(f"Index out of bounds index {index} and length {self.length}")
+            return
         self.length -= 1
-
-    def remove_at_end(self):
-        '''removes tail'''
-        temp = self.end
-        self.end = temp.prev
-        temp.prev.next = None
+        print(self.length)
 
     def add(self, song: Song, index=None) -> None:
         """
-		Add a song to the playlist.
-		If an index is specified, the song is inserted at that position.
-		Otherwise, it's appended to the end.
-		Args:
-			song (Song): song to add
-			index (int, optional): index where the song is being added. Defaults to None.
-		"""
+        Add a song to the playlist.
+        If an index is specified, the song is inserted at that position.
+        Otherwise, it's appended to the end.
+        Args:
+            song (Song): song to add
+            index (int, optional): index where the song is being added. Defaults to None.
+        """
         # check if the list is empty
-
+        
         # if not empty
-        # append or insert on the specific index
-        # self._get_song_at_index(index) will help retrive the song
+            # append or insert on the specific index
+            #self._get_song_at_index(index) will help retrive the song
         if not song:
-            print(
-                "\033[31mNo song provided. Cannot add to the playlist.\033[0m")
+            print("\033[31mNo song provided. Cannot add to the playlist.\033[0m")
             return
         else:
             if self.start is None:
@@ -108,43 +138,53 @@ class PlayList:
             elif index is None or index >= self.length:
                 self.end.next = song
                 song.prev = self.end
-                self.end = self.end.next
+                self.end = song
             else:
                 current = self._get_song_at_index(index=index)
-                current.prev.next = song
-                song.prev = current.prev
-                song.next = current
-                current.prev = song
+                if current is None:
+                    song.prev = self.end
+                    self.end.next = song
+                    self.end = song
+                elif current is self.start:
+                    song.next = current
+                    current.prev = song
+                    self.start = song
+                else:
+                    current.prev.next = song
+                    song.prev = current.prev
+                    song.next = current
+                    current.prev = song
 
             self.length += 1
             self.cur = self.start
-            print(self.start.name, self.cur.name, self.end.name)
+            print(self.length)
+
 
     def clear(self) -> None:
         """
-		Clear the entire playlist.
-		"""
+        Clear the entire playlist.
+        """
         self.start = None
         self.end = None
         self.cur = None
         self.length = 0
 
     # def sort(self, key: str) -> None:
-    #     """
-    #     Do this if time allows. This is not mendatory
+    #     """   
+    #     Do this if time allows. This is not mendatory     
     #     Sort the playlist based on the given key (e.g., 'name', 'views', 'run_time').
     #     - Implement a sorting algorithm suitable for doubly linked lists (e.g., insertion sort).
     #     Args:
     #         key (str): key to sort by
-
+            
     #     # Q: Which sorting algorithms have we learned that could be applied to a doubly-linked list? Which would be the most efficient? Are you  using the builtin sort by key method?
     #     """
     #     pass
 
     def reverse(self) -> None:
         """
-		Reverse the order of songs in the playlist.
-		"""
+        Reverse the order of songs in the playlist.
+        """
         new_start = self.end
         current = self.end
         while current.prev is not self.start:
@@ -158,47 +198,47 @@ class PlayList:
 
     def total_count(self) -> int:
         """
-		Return the total number of songs in the playlist.
+        Return the total number of songs in the playlist.
 
-		Returns:
-			int: total number of songs in the playlist
-		"""
+        Returns:
+            int: total number of songs in the playlist
+        """
         return self.length
-
+    
     def total_view(self) -> int:
         """
-		Return the total number of views of all songs in the playlist.
+        Return the total number of views of all songs in the playlist.
 
-		Returns:
-			int: total number of views of all songs in the playlist
-		"""
+        Returns:
+            int: total number of views of all songs in the playlist
+        """
         views = 0
         current = self.start
         while current is not None:
             views += current.views
             current = current.next
         return views
-
+    
     def total_runtime(self) -> int:
         """Return the total runtime of all songs in the playlist.
 
-		Returns:
-			int: total runtime of all songs in the playlist
-		"""
+        Returns:
+            int: total runtime of all songs in the playlist
+        """
         runtime = 0
         current = self.start
         while current is not None:
             runtime += current.run_time
             current = current.next
         return runtime
-
+    
     def display(self, chunk_size=10) -> None:
         """
-		Display songs in the playlist, chunk_size songs at a time.
+        Display songs in the playlist, chunk_size songs at a time.
 
-		Args:
-			chunk_size (int, optional): number of songs to display at a time. Defaults to 10.
-		"""
+        Args:
+            chunk_size (int, optional): number of songs to display at a time. Defaults to 10.
+        """
         # Check if playlist is empty, if it is empty return right away
         if not self.start:
             print("\033[35mThe playlist is empty!\033[0m")
@@ -207,21 +247,22 @@ class PlayList:
         current = self.start
         count = 0
 
-        # incase you want to show a chunk of song at a time and progress.
+        # incase you want to show a chunk of song at a time and progress. 
         while current and (count < chunk_size):
             print(current)
             print('test error')
             current = current.next
             count += 1
 
-
+        # your logic to show that chunk
+        pass
+                
     def play_current(self):
         """
-		Play the currently selected song.
-		"""
+        Play the currently selected song.
+        """
         if self.cur:
-            webbrowser.open(
-                f'https://www.youtube.com/watch?v={self.cur.youtube_id}')
+            webbrowser.open(f'https://www.youtube.com/watch?v={self.cur.youtube_id}')
             print(f'https://www.youtube.com/watch?v={self.cur.youtube_id}')
             print(f"Playing {self.cur.name} by {self.cur.artist}...")
         else:
@@ -229,8 +270,8 @@ class PlayList:
 
     def play_next(self):
         """
-		Goto or point to the next song in the playlist.
-		"""
+        Goto or point to the next song in the playlist.
+        """
         # go select the next song. No need to play here
         if self.cur is not None and self.cur.next is not None:
             self.cur = self.cur.next
@@ -238,8 +279,8 @@ class PlayList:
 
     def play_previous(self):
         """
-		Goto or point to the previous song in the playlist.        
-		"""
+        Goto or point to the previous song in the playlist.        
+        """
         # same as next
         if self.cur is not None and self.cur.prev is not None:
             self.cur = self.cur.prev
